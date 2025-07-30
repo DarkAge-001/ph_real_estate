d6ed1e929c4f92bc72b271ad7a3111804d22ee83
DarkAge-001 <godstimeatuegbe48@gmail.com>
2025-07-30 17:17:30 +0100

    corrected

--- backend/server.js
+++ backend/server.js
@@ -10,13 +10,13 @@ app.use(express.json());
 
 // Routes 
 app.use('/api/users', require('./routes/userRoutes'));
-app.use('/api/properties', requires('./routes/propertyRoutes'));
+app.use('/api/properties', require('./routes/propertyRoutes'));
 
 // MongoDB connect
-mongoose.connect(ProcessingInstruction.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log('MongoDB connected'))
     .catch((err) => console.log(err));
 
 // Server start
-const PORT = ProcessingInstruction.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
